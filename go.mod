16a223117b3a52b1217783cd3e94f86cbe3ea54a
jsandas <jsandas@users.noreply.github.com>
2025-07-26 13:14:43 -0600

    use copied modules

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/jsandas/tls-simulator
 go 1.24
 
 toolchain go1.24.5
+
+require golang.org/x/crypto v0.40.0
--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/binary"
 	"fmt"
+	"net"
+	"time"
+
+	"github.com/jsandas/tls-simulator/ftls"
 )
 
 const (
@@ -84,12 +89,129 @@ func main() {
 		tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
 		TLS_EMPTY_RENEGOTIATION_INFO_SCSV,
 	}
-	clientHello, err := buildClientHelloTLS12(tls.VersionTLS12, ciphers, nil)
+
+	clientMsg := ftls.ClientHelloMsg{
+		Vers:               tls.VersionTLS12,
+		CipherSuites:       ciphers,
+		CompressionMethods: []uint8{ftls.CompressionNone},
+		ServerName:         "localhost",
+		SessionId:          []byte{},
+		Random:             []byte{0x3a, 0x6e, 0x72, 0xcc, 0xf9, 0x3b, 0x29, 0xbb, 0xfb, 0x2d, 0xd0, 0xa3, 0x2b, 0x76, 0x3a, 0x9d, 0x28, 0x89, 0x11, 0xae, 0xfe, 0x4f, 0xf, 0x37, 0x6d, 0xce, 0xa0, 0x4a, 0xf, 0x8d, 0x6e, 0x15},
+		// SupportedVersions:  []uint16{tls.VersionTLS13, tls.VersionTLS12, tls.VersionTLS11, tls.VersionTLS10},
+		SupportedPoints: []uint8{ftls.PointFormatUncompressed},
+		SupportedCurves: []ftls.CurveID{ftls.X25519, ftls.CurveP256, ftls.CurveP384, ftls.CurveP521},
+		SupportedSignatureAlgorithms: []ftls.SignatureScheme{
+			ftls.PSSWithSHA512,
+			ftls.PKCS1WithSHA512,
+			ftls.ECDSAWithP521AndSHA512,
+			ftls.PSSWithSHA384,
+			ftls.PKCS1WithSHA384,
+			ftls.ECDSAWithP384AndSHA384,
+			ftls.PKCS1WithSHA256,
+			ftls.PSSWithSHA256,
+			ftls.ECDSAWithP256AndSHA256,
+			ftls.PKCS1WithSHA1,
+			ftls.ECDSAWithSHA1,
+		},
+		KeyShares: []ftls.KeyShare{
+			{
+				Group: ftls.CurveID(ftls.X25519),
+				Data:  []byte{0xed, 0x7, 0xea, 0x17, 0xf2, 0x33, 0x83, 0x69, 0x5, 0x94, 0x89, 0xc7, 0x9f, 0x57, 0x19, 0xcd, 0x6b, 0xcb, 0xe7, 0x22, 0x3d, 0xb1, 0x1b, 0x8b, 0xe1, 0x52, 0x1d, 0xc2, 0x49, 0x48, 0xe4, 0x3d},
+			},
+		},
+		AlpnProtocols: []string{"h2", "http/1.1"},
+	}
+
+	clientHello, err := clientMsg.MarshalMsg(false)
 	if err != nil {
 		panic(err)
 	}
-	err = sendClientHelloAndParseServerHello("localhost:443", clientHello)
+
+	// Wrap the handshake message in a TLS record
+	tlsRecord := make([]byte, 5+len(clientHello))
+	tlsRecord[0] = 0x16                                                  // Handshake record type
+	tlsRecord[1] = 0x03                                                  // TLS version major
+	tlsRecord[2] = 0x03                                                  // TLS version minor
+	binary.BigEndian.PutUint16(tlsRecord[3:5], uint16(len(clientHello))) // Record length
+	copy(tlsRecord[5:], clientHello)                                     // Handshake data
+
+	resp, err := sendClientHello("localhost:443", tlsRecord)
 	if err != nil {
 		fmt.Println("Handshake failed:", err)
 	}
+
+	serverHelloBytes, err := getServerHello(resp)
+	if err != nil {
+		fmt.Println("Failed to get ServerHello:", err)
+		return
+	}
+
+	serverHello := ftls.ServerHelloMsg{}
+
+	// fmt.Println("Received response, length:", len(serverHelloBytes))
+	// fmt.Printf("bytes: %x\n", serverHelloBytes[:5])
+	b2 := serverHello.Unmarshal(serverHelloBytes) // Skip the TLS record header (first 5 bytes)
+	if !b2 {
+		fmt.Println("Failed to unmarshal ServerHello")
+		return
+	}
+
+	fmt.Printf("ServerHello:\n%+v\n", serverHello)
+}
+
+func sendClientHello(addr string, clientHello []byte) ([]byte, error) {
+	conn, err := net.DialTimeout("tcp", addr, 5*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	conn.SetWriteDeadline(time.Now().Add(2 * time.Second))
+	_, err = conn.Write(clientHello)
+	if err != nil {
+		return nil, err
+	}
+
+	// Read the response (first record)
+	conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+	resp := make([]byte, 4096)
+	n, err := conn.Read(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse ServerHello
+	return resp[:n], nil
+}
+
+func getServerHello(data []byte) ([]byte, error) {
+	i := 0
+	for i+5 <= len(data) {
+		// Parse TLS record header
+		contentType := data[i]
+		// version := binary.BigEndian.Uint16(data[i+1 : i+3])
+		length := int(binary.BigEndian.Uint16(data[i+3 : i+5]))
+		if i+5+length > len(data) {
+			break // Malformed record
+		}
+		recordPayload := data[i+5 : i+5+length]
+		if contentType == 0x16 { // Handshake
+			// Parse handshake messages within this record
+			j := 0
+			for j+4 <= len(recordPayload) {
+				handshakeType := recordPayload[j]
+				handshakeLen := int(recordPayload[j+1])<<16 | int(recordPayload[j+2])<<8 | int(recordPayload[j+3])
+				if j+4+handshakeLen > len(recordPayload) {
+					break // Malformed handshake message
+				}
+				if handshakeType == ftls.TypeServerHello { // ServerHello
+					// Return the full handshake message (header + body)
+					return recordPayload[j : j+4+handshakeLen], nil
+				}
+				j += 4 + handshakeLen
+			}
+		}
+		i += 5 + length
+	}
+	return nil, fmt.Errorf("ServerHello not found in response")
 }
